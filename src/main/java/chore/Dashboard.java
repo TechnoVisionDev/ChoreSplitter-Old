3b1a42dd2b23dca3adfdf99d7be788ba9a12746e
Eric Yang <ericy61101@gmail.com>
2021-11-29 21:41:57 -0800

    initial add, delete, claim, completed chore

---
+++ src/main/java/chore/Dashboard.java
@@ -0,0 +1,30 @@
+package chore;
+import java.util.*;
+
+public class Dashboard {
+	private LinkedList<Chore> all_chores = new LinkedList<Chore>();
+	
+	
+	public Dashboard() {
+		
+	}
+	
+	
+	public void addChore(Chore chore) {
+		all_chores.add(chore);
+	}
+	
+	public void deleteChore(Chore chore) {
+		all_chores.remove(chore);
+	}
+	
+	public void claimChore(Chore chore) {
+		int index = all_chores.indexOf(chore);
+		all_chores.get(index).setState("In-progress");
+	}
+	
+	public void completedChore(Chore chore) {
+		int index = all_chores.indexOf(chore);
+		all_chores.get(index).setState("Complete");
+	}
+}
