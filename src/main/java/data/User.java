7d1d61987e0b769e87e3d8ef725e611ea891fce5
Emily Kim <emilyyk@usc.edu>
2021-11-30 19:24:40 -0800

    change user class to include points

--- src/main/java/data/User.java
+++ src/main/java/data/User.java
@@ -9,11 +9,13 @@ public class User {
 	private String email;
 	private String name;
 	private String password;
+	private int points;
 	
 	public User(String email, String name, String password) {
 		this.email = email;
 		this.name = name;
 		this.password = password;
+		points = 0;
 	}
 	
 	public String getEmail() {
@@ -26,5 +28,13 @@ public class User {
 	
 	public String getPassword() {
 		return password;
+	}
+	
+	public int getPoints() {
+		return points;
+	}
+	
+	public void addPoints(int p) {
+		points += p;
 	}
 }
