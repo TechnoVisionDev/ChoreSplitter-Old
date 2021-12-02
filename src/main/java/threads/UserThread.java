7479602490a6022fcdb9a925f5e658cfd7c78f30
Eric Yang <ericy61101@gmail.com>
2021-12-01 16:34:34 -0800

    update UserThread class

--- src/main/java/threads/UserThread.java
+++ src/main/java/threads/UserThread.java
@@ -2,15 +2,21 @@ package threads;
 
 import java.util.*;
 import chore.Chore;
+import data.User;
 
 public class UserThread extends Thread {
 	
+	private ArrayList<User> userList = new ArrayList<User>();
+	private User user;
 	private Chore chore;
 	private String state = "";
 	private LinkedList<Chore> completed_chores = new LinkedList<Chore>();
 	
-	public UserThread(Chore chore) {
-		this.chore = chore;
+	public UserThread(User _user, Chore _chore) {
+		this.user = _user;
+		this.chore = _chore;
+		userList.add(_user);
+		this.start();
 	}
 	
 	public void run() {
@@ -18,7 +24,8 @@ public class UserThread extends Thread {
 			state = chore.getState();
 			if (state.equals("Complete")) {
 				completed_chores.add(chore);
-				
+				user.addPoints(chore.getPoints());
+
 				break;
 			}
 		}
