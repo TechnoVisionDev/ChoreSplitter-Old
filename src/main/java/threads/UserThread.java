c885a284c48a9fae89e7ab7f2299e641ba041035
Eric Yang <ericy61101@gmail.com>
2021-12-01 16:07:37 -0800

    initial UserThread constructor and run() function

--- src/main/java/threads/UserThread.java
+++ src/main/java/threads/UserThread.java
@@ -1,5 +1,27 @@
 package threads;
 
-public class UserThread {
+import java.util.*;
+import chore.Chore;
 
+public class UserThread extends Thread {
+	
+	private Chore chore;
+	private String state = "";
+	private LinkedList<Chore> completed_chores = new LinkedList<Chore>();
+	
+	public UserThread(Chore chore) {
+		this.chore = chore;
+	}
+	
+	public void run() {
+		while (true) {
+			state = chore.getState();
+			if (state.equals("Complete")) {
+				completed_chores.add(chore);
+				
+				break;
+			}
+		}
+	}
+	
 }
