938385206710958c196c7a54ab9336f71063935c
Emily Kim <emilyyk@usc.edu>
2021-12-04 11:42:45 -0800

    Initial junit

---
+++ test/GroupTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <!-- https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api/5.8.2 -->
+	<dependency>
+    	<groupId>org.junit.jupiter</groupId>
+    	<artifactId>junit-jupiter-engine</artifactId>
+    	<version>5.8.1</version>
+    	<scope>test</scope>
+	</dependency>
+ */
+
+import org.junit.jupiter.api.Test;
+
+class GroupTest {
+
+	@Test
+	void groupCodeTest() {
+		String code = "";
+		
+		// assertEquals(false, servlets.GroupServlet.isValidCode(code));
+		
+		fail("Not yet implemented");
+	}
+
+}
