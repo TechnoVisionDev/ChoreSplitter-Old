cdbbadf58455d505bdd26d2bd10072cc03475823
Aaron Ahmed <aaron.ahmed71@gmail.com>
2021-12-06 15:52:55 -0800

    Added Password encryption

--- src/main/java/data/Database.java
+++ src/main/java/data/Database.java
@@ -1,9 +1,17 @@
 package data;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistries;
@@ -35,7 +43,7 @@ public class Database {
 	
 	private final MongoCollection<Document> users;
 	private final MongoCollection<Document> groups;
-	
+	private static final Random rand = new SecureRandom(); 
 	/**
      * Connect to database using MongoDB URI and
      * initialize any collections that don't exist.
@@ -75,11 +83,65 @@ public class Database {
 		Document doc = new Document();
 		doc.append("email", user.getEmail());
 		doc.append("name", user.getName());
-		doc.append("password", user.getPassword());
+		String salt = getSalt();
+		doc.append("salt", salt);
+		doc.append("encryptedPassword", encryptPassword(user.getPassword(), salt));
 		doc.append("points", user.getPoints());
 		doc.append("avatar", user.getAvatar());
 		users.insertOne(doc);
 	}
+	/**
+	 * Generates random salt value.
+	 * @return salt randomly generated salt value.
+	 */
+    private static String getSalt() {  
+    	byte[] salt = new byte[16];
+    	rand.nextBytes(salt);
+        return new String(salt);  
+    }     
+  
+	/**
+	 * Generates hashed value.
+	 * @param password Inputed password
+	 * @param salt salt value
+	 * @return hash value.
+	 */
+    private static byte[] hash(String password, String salt){  
+        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 10000, 256);  
+        try{  
+        	SecretKeyFactory fact = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");  
+            return fact.generateSecret(spec).getEncoded();  
+        }   
+        catch (NoSuchAlgorithmException ex) {
+            throw new IllegalStateException("Missing PBKDF2WithHmacSHA1 algorithm", ex);
+          }
+        catch (InvalidKeySpecException ex) {
+            throw new IllegalStateException("Invalid SecretKeyFactory", ex);
+         }    
+    }  
+    
+	/**
+	 * Generates encrypted password string.
+	 * @param password inputed password
+	 * @param salt salt value
+	 * @return encrypted password string.
+	 */
+    public static String encryptPassword(String password, String salt){            
+    	byte[] hashedPassword = hash(password, salt);   
+        return Base64.getEncoder().encodeToString(hashedPassword);  
+    }  
+      
+	/**
+	 * Verifies if inputed password matches encrypted password.
+	 * @param password password inputed by user
+	 * @param password encrypted password
+	 * @param salt salt value
+	 * @return true if inputed password matches encrypted password.
+	 */  
+    private static boolean verifyPassword(String userPass, String encryptedPass, String salt){          
+    	String userPassEncrypted = encryptPassword(userPass, salt);  
+    	return userPassEncrypted.equalsIgnoreCase(encryptedPass);  
+    } 
 	
 	/**
 	 * Retrieves user document to access values from.
@@ -101,7 +163,7 @@ public class Database {
 	public boolean validateUser(String email, String password) {
 		Document user = users.find(Filters.eq("email", email)).first();
 		if (user != null) {
-			return user.getString("password").equals(password);
+			return verifyPassword(password, user.getString("encryptedPassword"), user.getString("salt"));
 		}
 		return false;
 	}
