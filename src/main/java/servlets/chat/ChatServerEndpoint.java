22dd6bc11e025de3f1e6dbf8d1de69ead87a09e3
TechnoVisionDev <thomasanthonypeters@gmail.com>
2021-12-06 21:59:58 -0800

    Get ready to add message history support

--- src/main/java/servlets/chat/ChatServerEndpoint.java
+++ src/main/java/servlets/chat/ChatServerEndpoint.java
@@ -1,7 +1,9 @@
 package servlets.chat;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -19,19 +21,33 @@ import jakarta.websocket.server.ServerEndpoint;
 public class ChatServerEndpoint {
 	
     private static ConcurrentHashMap<String, Set<Session>> groupSessions = new ConcurrentHashMap<String, Set<Session>>();
+    private ConcurrentHashMap<String, List<String>> chatHistory = new ConcurrentHashMap<String, List<String>>();
+    
     private String group;
     
     @OnOpen
     public void onOpen(Session curSession) {
+    	// Get group code from user session
     	group = curSession.getRequestParameterMap().get("group").get(0);
+    	
+    	// Add user to group session
         if (!groupSessions.containsKey(group)) {
         	groupSessions.put(group, Collections.synchronizedSet(new HashSet<Session>()));
         }
         groupSessions.get(group).add(curSession);
+        
+        // Start chat history if doesn't exist for this group
+        if (!chatHistory.containsKey(group)) {
+        	chatHistory.put(group, Collections.synchronizedList(new ArrayList<String>()));
+        } else {
+        	// Send old messages to client
+        	chatHistory.get(group).forEach(m -> curSession.getAsyncRemote().sendText(m));
+        }
     }
             
     @OnClose
     public void onClose(Session curSession) {
+    	// Remove user from group session
         if (groupSessions.get(group).size() == 1) {
         	groupSessions.remove(group);
         } else {
@@ -41,6 +57,7 @@ public class ChatServerEndpoint {
     
     @OnMessage
     public void onMessage(String message, Session userSession) {
+    	chatHistory.get(group).add(message);
         for (Session ses : groupSessions.get(group)) {
         	ses.getAsyncRemote().sendText(message);
         }
